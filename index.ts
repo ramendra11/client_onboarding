7f646479e56f1c3e5750892afb21d9691efbcd75
ramendra11 <50911792+ramendra11@users.noreply.github.com>
2019-10-03 13:09:36 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { TokenSigner } from 'jsontokens'
 
-const getSignPayload = async(payload1: any) => {
-    const signerKeyHex = "8df46e931a1917ea7a2b6c874b679cd903dca44f16c949659c9aa03849425da3"
-    return new TokenSigner('ES256K', signerKeyHex).sign(payload1)
+const getSignPayload = async(payload) => {
+    const signerKeyHex = "your-private_key"
+    return new TokenSigner('ES256K', signerKeyHex).sign(payload)
 }
-const payload1 = {"ram": "ramne"}
-console.log(getSignPayload(payload1))
+const payload = {"ramendra": "ramendra"} // The paylod to be signed 
+console.log(getSignPayload(payload))
